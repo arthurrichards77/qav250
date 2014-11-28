a4ac277fc4b4c2fb7759bfa524c8ecf25f51332b
User Account <user@IT004409.(none)>
2014-11-28 11:42:56 +0000

    Re-arranged GUI layout

--- scripts/control_gui.py
+++ scripts/control_gui.py
@@ -14,19 +14,19 @@ class App:
 		frame=Frame(master)
 		frame.pack()
 
-		#Create publisher for push point
-		self.push_pub = rospy.Publisher('goal_point', Pose)
 		#Create publisher for goal point
 		self.goal_pub = rospy.Publisher('goal_point', Pose)
+
 		#Create publisher to freeze integator
                 self.freeze_int_pub = rospy.Publisher('freeze_int', Bool)
+
 		#Initial position
 		self.position = Pose()
 		self.position.position.x = rospy.get_param('init_x', 0.0)
 		self.position.position.y = rospy.get_param('init_y', 0.0)
 		self.position.position.z = 0.8
 
-		#Get Min/Max values for x,y,z - Question mark over it is better to use these, or have some kind of check that a message has been set.
+		#Get Min/Max values for x,y,z
 		self.min_x = rospy.get_param('/min_x', -5.0)
 		self.max_x = rospy.get_param('/max_x', 5.0)
 		self.min_y = rospy.get_param('/min_y', -5.0)
@@ -40,113 +40,122 @@ class App:
 		self.z_step = rospy.get_param('/z_step', 0.1)
 
 		#Get dwell time for drop manouevre
-		self.drop_dwell = rospy.get_param('/drop_dwell', 0.3)
+		self.drop_dwell = rospy.get_param('/drop_dwell', 0.6)
+		#Get up drell time for drop manoeuvre
+		#self.up_dwell = rospy.get_param('/up_dwell', 0.2)
 
 		#Make quit button
 		self.quit_button = Button(frame, text="Quit", command=frame.quit)
-		self.quit_button.grid(row=0, column=1)
+		self.quit_button.grid(row=1000, column=1000)
 
 		#Make stop button
 		self.stop_button = Button(frame, text="Stop", command=self.stop_callback)
-		self.stop_button.grid(row=0, column=0)
+		self.stop_button.grid(row=101, column=2)
 
 		#Directional push buttons
 		self.push_buttons = list()
-
+		#+z
 		self.up_button = Button(frame, text="+z", command=lambda m="+z": self.push_callback(m))
-		self.up_button.grid(row=1, column=0)
+		self.up_button.grid(row=0, column=3)
 		self.push_buttons.append(self.up_button)
-
+		#-z
 		self.down_button = Button(frame, text="-z", command=lambda m="-z": self.push_callback(m))
-		self.down_button.grid(row=1, column=1)
+		self.down_button.grid(row=2, column=3)
 		self.push_buttons.append(self.down_button)
-
+		#+y
 		self.left_button = Button(frame, text="+y", command=lambda m="+y": self.push_callback(m))
-		self.left_button.grid(row=2, column=0)
+		self.left_button.grid(row=0, column=1)
 		self.push_buttons.append(self.left_button)
-
+		#-y
 		self.right_button = Button(frame, text="-y", command=lambda m="-y": self.push_callback(m))
 		self.right_button.grid(row=2, column=1)
 		self.push_buttons.append(self.right_button)
-
+		#+x
 		self.forward_button = Button(frame, text="+x", command=lambda m="+x": self.push_callback(m))
-		self.forward_button.grid(row=3, column=0)
+		self.forward_button.grid(row=1, column=2)
 		self.push_buttons.append(self.forward_button)
-
+		#-x
 		self.back_button = Button(frame, text="-x", command=lambda m="-x": self.push_callback(m))
-		self.back_button.grid(row=3, column=1)
+		self.back_button.grid(row=1, column=0)
 		self.push_buttons.append(self.back_button)
 
 		#Buttons for landing / take-off
-		self.land_button = Button(frame, text="Land", command=self.land_callback)
-		self.land_button.grid(row=4, column=0)
+		self.land_button = Button(frame, text="Take_off", command=self.takeoff_callback)
+		self.land_button.grid(row=100, column=0)
 
-		self.takeoff_button = Button(frame, text="Take-off", command=self.takeoff_callback)
-		self.takeoff_button.grid(row=4, column=1)
+		self.takeoff_button = Button(frame, text="Land", command=self.land_callback)
+		self.takeoff_button.grid(row=101, column=1)
+
+		#Button to go to position above target
+		self.add_pos_button = Button(frame, text="Go above pad", command=self.add_goal)
+		self.add_pos_button.grid(row=100, column=1)
+
+		#Drop on to target
+		self.drop_button = Button(frame, text="Drop", command=lambda m="-drop": self.push_callback(m))
+		self.drop_button.grid(row=100, column=2)
+
+		#Button to return to landing pad
+		self.add_pos_button = Button(frame, text="Go above pad", command=self.add_goal)
+		self.add_pos_button.grid(row=101, column=0)
 
 		#Fixed positional input entry boxes
 		#Create label
 		self.x_label = Label(frame, text='X position')
-		self.x_label.grid(row=1, column=2, padx=10, pady=10)
-		#Bind validation to entry box when object is created
+		self.x_label.grid(row=0, column=100, padx=10, pady=10)
+		#Create entry box
 		vcmd = (frame.register(self.validate_goal), '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
 		self.x_entry = Entry(frame, validate="key", validatecommand=vcmd)
-		self.x_entry.grid(row=1, column=3, padx=10, pady=10)
+		self.x_entry.grid(row=2, column=100, padx=10, pady=10)
 
+		#Create label
 		self.y_label = Label(frame, text='Y position')
-		self.y_label.grid(row=2, column=2, padx=10, pady=10) 
+		self.y_label.grid(row=0, column=101, padx=10, pady=10)
+		#Create entry box
 		self.y_entry = Entry(frame, validate="key", validatecommand=vcmd)
-		self.y_entry.grid(row=2, column=3, padx=10, pady=10)
+		self.y_entry.grid(row=2, column=101, padx=10, pady=10)
 
+		#Create label
 		self.z_label = Label(frame, text='Z position')
-		self.z_label.grid(row=3, column=2, padx=10, pady=10) 
+		self.z_label.grid(row=0, column=102, padx=10, pady=10)
+		#Create entry box
 		self.z_entry = Entry(frame, validate="key", validatecommand=vcmd)
-		self.z_entry.grid(row=3, column=3, padx=10, pady=10)
+		self.z_entry.grid(row=2, column=102, padx=10, pady=10)
 
+		#Send command button
 		self.send_pos_button = Button(frame, text="Send", command=self.send_position)
-		self.send_pos_button.grid(row=4, column=2)
+		self.send_pos_button.grid(row=4, column=100)
 
+		#Dispay status of command
 		self.send_status_text = StringVar()
 		self.send_status_text.set('')
 		self.send_status_label = Label(frame, textvariable=self.send_status_text)
-		self.send_status_label.grid(row=4, column=3)
-
-		self.add_pos_button = Button(frame, text="Add", command=self.add_goal)
-		self.add_pos_button.grid(row=5, column=2)
-
-		self.drop_button = Button(frame, text="Drop", command=lambda m="-drop": self.push_callback(m))
-		self.drop_button.grid(row=5, column=3)
+		self.send_status_label.grid(row=4, column=101)
 
 		#Position feedback
 		self.x_pos_text = StringVar()
 		self.x_pos_text.set('0.0')
 		self.x_pos_label = Label(frame, textvariable=self.x_pos_text)
-		self.x_pos_label.grid(row=6, column=5)
-		self.x_pos_label = Label(frame, text="X")
-		self.x_pos_label.grid(row=5, column=5)
+		self.x_pos_label.grid(row=1, column=100)
 
 		self.y_pos_text = StringVar()
 		self.y_pos_text.set('0.0')
 		self.y_pos_label = Label(frame, textvariable=self.y_pos_text)
-		self.y_pos_label.grid(row=6, column=6)
-		self.y_pos_label = Label(frame, text="Y")
-		self.y_pos_label.grid(row=5, column=6)
+		self.y_pos_label.grid(row=1, column=101)
 
 		self.z_pos_text = StringVar()
 		self.z_pos_text.set('0.0')
 		self.z_pos_label = Label(frame, textvariable=self.z_pos_text)
-		self.z_pos_label.grid(row=6, column=7)
-		self.z_pos_label = Label(frame, text="Z")
-		self.z_pos_label.grid(row=5, column=7)
+		self.z_pos_label.grid(row=1, column=102)
+
 
 		#Drop dwell entry
 		self.dwell_label = Label(frame, text='Drop dwell time')
-		self.dwell_label.grid(row=10, column=6, padx=10, pady=10) 
+		self.dwell_label.grid(row=100, column=100, padx=10, pady=10) 
 		self.dwell_entry = Entry(frame, validate="key", validatecommand=vcmd)
-		self.dwell_entry.grid(row=10, column=7, padx=10, pady=10)
+		self.dwell_entry.grid(row=100, column=101, padx=10, pady=10)
 
 		self.update_dwell = Button(frame, text="Update dwell", command=self.update_dwell)
-		self.update_dwell.grid(row=10, column=5)
+		self.update_dwell.grid(row=100, column=102)
 
 	def update_dwell(self):
 		try:
@@ -194,6 +203,10 @@ class App:
 				self.position.orientation.w = 0.1
 				self.send_goal()
 				time.sleep(self.drop_dwell)
+				#self.position.position.z = 2.0
+				#self.position.orientation.w = 0.1
+				#self.send_goal()
+				#time.sleep(self.up_dwell)
 				self.position.position.z = old_pos_z
 				self.position.orientation.w = 0.1
                                 self.freeze_int_pub.publish(False)
@@ -260,11 +273,15 @@ class App:
 		#Allow a single dot character
 		elif S == '.' and P.count('.') <= 1:
 			return True
+		#Allow deletion
+		elif P == '':
+			return True
 		#Reject everything else
 		else:
 			return False
 
 	def send_goal(self):
+
 		self.goal_pub.publish(self.position)
 		self.x_pos_text.set("%2.2f" % self.position.position.x)
 		self.y_pos_text.set("%2.2f" % self.position.position.y)
